c854581ac5e00671deba70e70086754330916c02
10152737 <yongtae89.jeon@kt.com>
2024-01-16 16:47:42 +0900

    version 변경

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,6 +7,6 @@ object Versions {
 
     const val minSdk = 23
     const val targetSdk = 33
-    const val versionCode = 24
-    const val versionName = "3.0.1"
+    const val versionCode = 25
+    const val versionName = "3.0.2"
 }
